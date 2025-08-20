a3ae5d2790dc7ec49404f79513a3b74d88f709ee
haileyeong <haileyeong@gmail.com>
2025-08-20 14:15:24 +0900

    Form3

---
+++ Form3.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace myproject1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNum1 = new TextBox();
+            btnPlus = new Button();
+            btnMinus = new Button();
+            btnMultiply = new Button();
+            btnDivide = new Button();
+            txtNum2 = new TextBox();
+            lblResult = new Label();
+            SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(64, 75);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(100, 23);
+            txtNum1.TabIndex = 0;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Location = new Point(245, 74);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(75, 23);
+            btnPlus.TabIndex = 1;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = true;
+            btnPlus.Click += btnPlus_Click;
+            // 
+            // btnMinus
+            // 
+            btnMinus.Location = new Point(245, 103);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(75, 23);
+            btnMinus.TabIndex = 1;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = true;
+            btnMinus.Click += btnMinus_Click;
+            // 
+            // btnMultiply
+            // 
+            btnMultiply.Location = new Point(245, 132);
+            btnMultiply.Name = "btnMultiply";
+            btnMultiply.Size = new Size(75, 23);
+            btnMultiply.TabIndex = 1;
+            btnMultiply.Text = "x";
+            btnMultiply.UseVisualStyleBackColor = true;
+            btnMultiply.Click += btnMultiply_Click;
+            // 
+            // btnDivide
+            // 
+            btnDivide.Location = new Point(245, 161);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new Size(75, 23);
+            btnDivide.TabIndex = 1;
+            btnDivide.Text = "/";
+            btnDivide.UseVisualStyleBackColor = true;
+            btnDivide.Click += btnDivide_Click;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(395, 75);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(100, 23);
+            txtNum2.TabIndex = 0;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Font = new Font("한컴 말랑말랑 Bold", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblResult.Location = new Point(245, 247);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(75, 31);
+            lblResult.TabIndex = 2;
+            lblResult.Text = "결과값";
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(557, 395);
+            Controls.Add(lblResult);
+            Controls.Add(btnDivide);
+            Controls.Add(btnMultiply);
+            Controls.Add(btnMinus);
+            Controls.Add(btnPlus);
+            Controls.Add(txtNum2);
+            Controls.Add(txtNum1);
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNum1;
+        private Button btnPlus;
+        private Button btnMinus;
+        private Button btnMultiply;
+        private Button btnDivide;
+        private TextBox txtNum2;
+        private Label lblResult;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myproject1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int result = num1 + num2;
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int result = num1 - num2;
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int result = num1 * num2;
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int result = num1 / num2;
+            lblResult.Text = result.ToString();
+        }
+    }
+}
