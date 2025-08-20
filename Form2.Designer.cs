6d3102c2ff07187ef52b6008d98733aca541cc0c
haileyeong <haileyeong@gmail.com>
2025-08-20 13:11:37 +0900

    Form2 and update Program.cs

---
+++ Form2.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace myproject1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnInt = new Button();
+            btnDouble = new Button();
+            btnString = new Button();
+            lblResult = new Label();
+            SuspendLayout();
+            // 
+            // btnInt
+            // 
+            btnInt.Location = new Point(102, 79);
+            btnInt.Name = "btnInt";
+            btnInt.Size = new Size(97, 41);
+            btnInt.TabIndex = 0;
+            btnInt.Text = "정수형 출력";
+            btnInt.UseVisualStyleBackColor = true;
+            btnInt.Click += btnInt_Click;
+            // 
+            // btnDouble
+            // 
+            btnDouble.Location = new Point(102, 167);
+            btnDouble.Name = "btnDouble";
+            btnDouble.Size = new Size(97, 41);
+            btnDouble.TabIndex = 0;
+            btnDouble.Text = "실수형 출력";
+            btnDouble.UseVisualStyleBackColor = true;
+            btnDouble.Click += btnDouble_Click;
+            // 
+            // btnString
+            // 
+            btnString.Location = new Point(102, 250);
+            btnString.Name = "btnString";
+            btnString.Size = new Size(97, 41);
+            btnString.TabIndex = 0;
+            btnString.Text = "문자열 출력";
+            btnString.UseVisualStyleBackColor = true;
+            btnString.Click += btnString_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Font = new Font("한컴 말랑말랑 Bold", 14.2499981F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblResult.Location = new Point(356, 175);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(46, 25);
+            lblResult.TabIndex = 1;
+            lblResult.Text = "결과";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(639, 404);
+            Controls.Add(lblResult);
+            Controls.Add(btnString);
+            Controls.Add(btnDouble);
+            Controls.Add(btnInt);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnInt;
+        private Button btnDouble;
+        private Button btnString;
+        private Label lblResult;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myproject1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            // 실행 프로그램 작성
+            int iValue = 100;
+            lblResult.Text = iValue.ToString();
+        }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            // 실행 프로그램 작성
+            double dValue = 11.01;
+            lblResult.Text = dValue.ToString();
+        }
+
+        private void btnString_Click(object sender, EventArgs e)
+        {
+            // 실행 프로그램 작성
+            string sValue = "C# 프로그래밍 기초";
+            lblResult.Text = sValue;
+        }
+    }
+}
