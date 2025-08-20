d0c45f56ea38749e0aeed90da5ff7c4b1da160e2
haileyeong <haileyeong@gmail.com>
2025-08-20 10:49:27 +0900

    test form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(88, 252);
+            button1.Name = "button1";
+            button1.Size = new Size(156, 49);
+            button1.TabIndex = 0;
+            button1.Text = "글자 출력";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(299, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 1;
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Location = new Point(367, 252);
+            button2.Name = "button2";
+            button2.Size = new Size(156, 49);
+            button2.TabIndex = 2;
+            button2.Text = "글자 지우기";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(627, 361);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,17 @@ namespace myproject1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 실행 프로그램 작성
+            label1.Text = "C# 프로그래밍";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 실행 프로그램 작성
+            label1.ResetText();
+        }
     }
 }
